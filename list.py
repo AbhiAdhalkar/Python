57ca238e79f1f9a123876ebf46e631d7cbd76d73
AbhiAdhalkar <adhalkarabhishek1@gmail.com>
2025-11-17 21:34:54 +0530

    list file added

---
+++ list.py
@@ -0,0 +1,11 @@
+list = ["Abhishek","Omkar","Saiprasad","Snehal","Akshay","Shweta","Vaibhav","Ajit"]
+
+#list.remove[1]
+
+#for i in list:
+#    print(i)
+
+print(type(list))
+list.append("Viramalla")
+print(len(list))
+
